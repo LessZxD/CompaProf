65e699092746e6f5a51edac8e4bac1c51e969613
Chronostasiss <ahmad.nizham416@gmail.com>
2024-12-26 15:31:53 +0700

    changed hero

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "About Page | Free Next.js Template for Startup and SaaS",
   description: "This is About Page for Startup Nextjs Template",
   // other metadata
-};
+};  
 
 const AboutPage = () => {
   return (
