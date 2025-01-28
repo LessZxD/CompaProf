675d6431b6f9e2af4549b924529941bd52720062
LessZxD <ekosw2172003@gmail.com>
2025-01-28 16:30:55 +0700

    few changes on whatsapp

--- src/config/whatsapp.ts
+++ src/config/whatsapp.ts
@@ -1,5 +1,5 @@
 // config/whatsapp.ts
 // WHATSAPP CONFIG
-const phoneNumber = "+6287887152685";
-const defaultMessage = "Bikinin website dong";
+const phoneNumber = "+6281298529573";
+const defaultMessage = "Ingin bertanya mengenai website dong";
 export const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(defaultMessage)}`;
